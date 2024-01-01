44d8e69a02cbd1c1eae08edf64db9b36a8965ec4
minky <kimminkyeu@naver.com>
2024-01-01 16:30:42 +0900

    Shared lib link test

--- source/main.cpp
+++ source/main.cpp
@@ -13,7 +13,7 @@ private: int32_t _data;
 int main()
 {
     // CM_ASSERT_COMPILE("김민규", sizeof(int) == 4, "compile assert test입니다!");
-    // CM_ASSERT_NOTE("김민규", false, "assert test입니다!");
+    CM_ASSERT_NOTE("김민규", false, "assert test입니다!");
     // CM::test_speak();
 
     std::cout << "Hello, from chimera!\n";
